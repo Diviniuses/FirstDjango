4f9d6bf39280366bd1bc2a03eec17924973a8f2b
Loza Vladislav <lozavladislav1337@gmail.com>
2025-04-28 22:44:22 +0300

    zadanie_1_2

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 
 def home(request):
     text = """
-    <h1>"Изучаем джанго"<h1>
-    <strong>Автор<strong>:
+    <h1>Изучаем Django</h1>
+    <strong>Автор</strong>
+    """
+    return HttpResponse(text)
+
+def about_page(request):
+    text = """
+    <h1>Имя: Иван</h1>
+    <p>Отчество: Петрович</p>
+    <p>Фамилия: Иванов</p>
+    <p>Телефон: 8-923-600-01-02</p>
+    <p>Email: vasya@mail.ru</p>
     """
     return HttpResponse(text)
