f087b062bfc0145e7ea254734f77b04848753f02
Loza Vladislav <lozavladislav1337@gmail.com>
2025-04-28 22:51:34 +0300

    v processe_3_4

--- MainApp/views.py
+++ MainApp/views.py
@@ -17,3 +17,19 @@ def about_page(request):
     <p>Email: vasya@mail.ru</p>
     """
     return HttpResponse(text)
+
+items = [
+    {"id": 1, "name": "Кроссовки abibas"},
+    {"id": 2, "name": "Куртка кожаная"},
+    {"id": 3, "name": "Соса-сola 1 литр"},
+    {"id": 4, "name": "Картофель фри"},
+    {"id": 5, "name": "Кепка"},
+]
+
+def item_detail(request, item_id):
+    for _ in items:
+        if _['id'] == item_id:
+            return HttpResponse(f"<h1>{item['name']}</h1>")
+    
+    # Если товар не найден 
+    return HttpResponse(f"<h1>Товар с id={item_id} не найден</h1>", status=404)
