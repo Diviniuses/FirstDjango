35a6bbb02f751fc8425e04be897655e132d47392
Loza Vladislav <lozavladislav1337@gmail.com>
2025-06-08 19:50:48 +0300

    Рефакторинг шаблонов: добавлены именованные URL, разделение на header и footer

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from MainApp import views
 
 urlpatterns = [
-    path('', views.home),
-    path('about/', views.about_page),
-    path('item/<int:item_id>/', views.item_detail),
-    path('items/', views.items_list),
+    path('', views.home, name='home'),
+    path('about/', views.about_page, name='about'),
+    path('item/<int:item_id>/', views.item_detail, name='item_detail'),
+    path('items/', views.items_list, name='items_list'),
 ]
 
 
--- MainApp/models.py
+++ MainApp/models.py
@@ -4,6 +4,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     quantity = models.IntegerField(default=0)
     description = models.TextField(default='')
+    brand = models.CharField(max_length=100, default='')
 
     def __str__(self):
         return self.name
